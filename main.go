ae0b55fdde0ef440e3ff9adcc16ecd0dc0933739
pollenjp <polleninjp@gmail.com>
2022-08-20 19:44:34 +0900

    Set location for timezone

--- main.go
+++ main.go
@@ -5,12 +5,24 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/pollenjp/pomodoro-bot/app"
 	_ "github.com/pollenjp/pomodoro-bot/app/pomodoro"
 )
 
+
 func init() {
+	location := os.Getenv("TZ")
+	default_offset := 9*60*60
+	if len(location) == 0 {
+		location = "Asia/Tokyo"
+	}
+	loc, err := time.LoadLocation(location)
+	if err != nil {
+		loc = time.FixedZone(location, default_offset)
+	}
+	time.Local = loc
 }
 
 func main() {
